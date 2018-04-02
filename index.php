73bc7eb88db763d2a0bc7d03a2b47f62043b9a06
KadyrovDaniyar <37533543+KadyrovDaniyar@users.noreply.github.com>
2018-04-02 16:36:13 +0600

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,17 @@
 //подключили библиотеку
 require_once 'simple_html_dom.php';
 //скачали страничку
-$page = file_get_html('http://xdan.ru');
+$page = file_get_html('http://vse-sto.kz/almaty/sto/?page=1');
 //проверка нашли ли хотя бы 1 блок img и не пустая ли страница
 if($page->innertext!='' and count($data->find('img')))
   //для всех элементов найдём элементы img
   foreach($data->find('img') as $img)
+    //выведем данный элемент
+    echo $a->innertext;
+	foreach($data->find('item-link') as $item)
+    //выведем данный элемент
+    echo $a->innertext;
+	foreach($data->find('service-info') as $service)
     //выведем данный элемент
     echo $a->innertext;
 ?>
